ecb7326f8bca0127acff5a2d59f7c04163b8d863
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-30 11:30:21 +0700

    add the package

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+	reactStrictMode: true,
+	compilerComponents: {
+		styledComponents: true,
+	},
+}
 module.exports = nextConfig
