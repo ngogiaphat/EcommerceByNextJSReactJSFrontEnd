3e52b8a3d613a8baa0a7edd7f5072b372e937471
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-30 18:18:50 +0700

    complete pages

---
+++ pages/products.js
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import Center from "@/components/Center";
+import Title from "@/components/Title";
+import ProductsGrid from "@/components/ProductsGrid";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
+import styled from "styled-components";
+export default function ProductsPage({products}){
+	return(
+		<>
+			<Header/>
+			<Center>
+				<Title>All products</Title>
+				<ProductsGrid products = {products}/>
+			</Center>
+		</>
+	);
+};
+export async function getServerSideProps(){
+	await mongooseConnect();
+	const products = await Product.find({}, null, {sort: {'_id': -1}});
+	return {
+		props: {
+			products: JSON.parse(JSON.stringify(products)),
+		},
+	};
+};
