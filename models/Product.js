f5a0aea230757fb895bb69b6f7eb2a7aa8c7c445
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-30 13:19:13 +0700

    complete models

---
+++ models/Product.js
@@ -0,0 +1,14 @@
+import mongoose, {model, models, Schema} from "mongoose";
+const ProductSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: String,
+	price: {
+		type: Number,
+		required: true,
+	},
+}, {
+	timeStamp: true,
+});
